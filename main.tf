d68ad970db33a2ae90ab364b7ab912b5e1afaefc
Loren Yeung <loren.w.yeung@gmail.com>
2026-05-14 18:51:41 -0700

    fix: remove duplicate terraform/variable declarations in main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-terraform {
-  required_providers {
-    harness = { source = "harness/harness" }
-    random  = { source = "hashicorp/random" }
-    null    = { source = "hashicorp/null" }
-    time    = { source = "hashicorp/time" }
-  }
-}
-
 module "base" {
   source        = "git::https://github.com/lorenyeung/test-module-registry.git?ref=v1.0.11"
   num_resources = var.num_resources
-}
-
-variable "num_resources" {
-  type    = number
-  default = 2
 }
--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,7 @@
 terraform {
   required_providers {
-    harness = {
-      source = "harness/harness"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    time = {
-      source = "hashicorp/time"
-    }
+    random = { source = "hashicorp/random" }
+    null   = { source = "hashicorp/null" }
+    time   = { source = "hashicorp/time" }
   }
 }
