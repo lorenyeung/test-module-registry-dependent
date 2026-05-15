5ea476c997117e339910699e64aed5b1b892e2b5
Loren Yeung <loren.w.yeung@gmail.com>
2026-05-14 18:58:53 -0700

    add examples/basic with init-apply-destroy pattern for integration-test command

---
+++ examples/basic/outputs.tf
@@ -0,0 +1,7 @@
+output "resource_count" {
+  value = module.base.resource_count
+}
+
+output "random_ids" {
+  value = module.base.random_ids
+}
--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    harness = { source = "harness/harness" }
-  }
-}
-
 module "base" {
   source        = "git::https://github.com/lorenyeung/test-module-registry.git?ref=main"
   num_resources = var.num_resources
-}
-
-variable "num_resources" {
-  type    = number
-  default = 2
 }
